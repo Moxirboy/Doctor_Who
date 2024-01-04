d0f04dbe1489498e6ff47dc29113b237cb8be0e7
moxirboy <mox777boy@gmail.com>
2024-01-04 16:02:18 +0500

    fixed: programs

--- migrations/000001_user.up.sql
+++ migrations/000001_user.up.sql
@@ -3,8 +3,7 @@
 CREATE TYPE customrole AS ENUM ('doctor', 'user');
 CREATE TABLE users (
                        id SERIAL PRIMARY KEY,
-                       phone_number VARCHAR(12) NOT NULL,
-                       password VARCHAR(128) NOT NULL,
+                       phone_number VARCHAR(15) NOT NULL,
                        role customrole NOT NULL DEFAULT 'user',
                        created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
                        updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
