02c0e50c51f0cb935f6d59e8d5194bee6be20046
mirva777 <118059890+mirva777@users.noreply.github.com>
2024-01-07 17:26:38 +0500

    Update 000006_exercise.up.sql

--- migrations/000006_exercise.up.sql
+++ migrations/000006_exercise.up.sql
@@ -5,5 +5,6 @@ CREATE TABLE exercise (
                           program_id INT,
                           name VARCHAR(255),
                           info varchar(255),
+                          link VARCHAR(255),
                           CONSTRAINT fk_exercise_program FOREIGN KEY (program_id) REFERENCES programs(id)
 );
